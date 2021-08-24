4e284a7dffd86da62661d1d8f2e14bdfd3cbaf02
Mauro Santos <mauro.santos@totvs.com.br>
2021-08-24 16:29:58 -0300

    style: 💄 updates contact tile style

--- lib/components/dashboard_tile.dart
+++ lib/components/dashboard_tile.dart
@@ -15,9 +15,10 @@ class DashboardTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 16.0),
       child: Material(
         color: Theme.of(context).primaryColor,
+        borderRadius: BorderRadius.circular(50.0),
         child: InkWell(
           onTap: () {
             Navigator.of(context).push(
@@ -27,27 +28,18 @@ class DashboardTile extends StatelessWidget {
             );
           },
           child: Container(
-            height: 100.0,
-            width: 150.0,
+            height: 80.0,
+            width: double.maxFinite,
             color: Colors.transparent,
             padding: EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Icon(
-                  icon,
+            child: Center(
+              child: Text(
+                title,
+                style: TextStyle(
                   color: Colors.white,
-                  size: 24.0,
+                  fontSize: 24.0,
                 ),
-                Text(
-                  title,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16.0,
-                  ),
-                ),
-              ],
+              ),
             ),
           ),
         ),
